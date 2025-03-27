e65c82b1bf9e2f0afd9c63d8dfd1dcdcbd3cc24b
Damon Blais <damon.blais@gmail.com>
2025-03-27 04:48:50 -0700

    Add unused function with unreachablecode for testing dead code finder

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+// unusedFunction is never called and contains unreachable code
+func unusedFunction() {
+	fmt.Println("This function is never called")
+	return
+	fmt.Println("This code is unreachable")
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 }
